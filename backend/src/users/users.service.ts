c097b02f0ff625d99ca51a07bf56ca1e7e24700c
Terence MAYOMBO <terencemayombo@MacBook-Pro-de-Terence.local>
2026-03-24 15:32:51 +0100

    add resources users class validator, bcrypt and swagger etc.

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = this.usersRepository.create(createUserDto);
+    user.password = hashedPassword;
+    return await this.usersRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.findOne(id);
+    Object.assign(user, updateUserDto);
+    return await this.usersRepository.save(user);
+  }
+
+  async remove(id: number): Promise<{ message: string }> {
+    const user = await this.findOne(id);
+    await this.usersRepository.remove(user);
+    return { message: `User with ID ${id} deleted successfully` };
+  }
+}
